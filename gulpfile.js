e054595b58d5e76388df6071ee9894cb39762fe8
Joern Bernhardt <jb@campudus.com>
2016-02-19 18:53:54 +0100

    Add styling via gulp-sass
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var del = require('del');
 
 gulp.task('default', ['build']);
 
-gulp.task('build', buildTask);
+gulp.task('build', ['build:assets', 'build:sass']);
+gulp.task('build:assets', buildAssetsTask);
+gulp.task('build:sass', buildSassTask);
 gulp.task('clean', cleanTask);
 
-function buildTask() {
+function buildAssetsTask() {
   /* takes all files it finds under src/assets/ */
   return gulp.src('src/assets/**')
     /* ... and writes them into the out folder */
     .pipe(gulp.dest('out'));
+}
+
+function buildSassTask() {
+  /* takes the main.scss file */
+  return gulp.src('src/styles/main.scss')
+    /* transforms it through sass */
+    .pipe(sass({outputStyle: 'expanded'}))
+    /* ... and writes them into the out folder */
+    .pipe(gulp.dest('out/css'));
 }
 
 function cleanTask() {
