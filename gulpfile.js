fa06589da3ec0ca8be4f344b37ddfac36f43e376
Joern Bernhardt <jb@campudus.com>
2016-02-19 17:13:14 +0100

    Create a build and a cleanup task
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var del = require('del');
+
+gulp.task('default', 'build');
+
+gulp.task('build', buildTask);
+gulp.task('clean', cleanTask);
+
+function buildTask() {
+  /* takes all files it finds under src/assets/ */
+  return gulp.src('src/assets/**')
+    /* ... and writes them into the out folder */
+    .pipe(gulp.dest('out'));
+}
+
+function cleanTask() {
+  /* del returns a promise, so gulp will know when it's done doing its magic. */
+  return del('out');
+}
