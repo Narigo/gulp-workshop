3a21b82486c1204ed31135b923521df1619646ac
Joern Bernhardt <jb@campudus.com>
2016-02-20 05:09:36 +0100

    Add index for blog posts
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,11 @@ var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync');
 var wrap = require('gulp-wrap');
 var rename = require('gulp-rename');
+var through = require('through2');
+var merge = require('merge-stream');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var path = require('path');
 var del = require('del');
 
 gulp.task('default', ['build']);
@@ -28,15 +33,51 @@ function buildAssetsTask() {
 
 function buildBlogTask() {
   /* takes all json files it finds under src/blog/posts */
-  return gulp.src('src/blog/posts/*.json')
-    /* wrap uses the data with a template */
+  var posts = gulp.src('src/blog/posts/*.json')
+    .pipe(plumber(function (err) {
+      console.log('[error]', err);
+      this.push(null);
+    }));
+
+  /* split the stream into two paths */
+  var processedPosts = posts
+  /* create a post from the data */
     .pipe(wrap({src : 'src/blog/post-template.html'}))
     /* rename the (.json) posts into .html files */
     .pipe(rename({extname : '.html'}))
     /* ... and writes them into the out folder */
-    .pipe(gulp.dest('out/blog'))
-    /* tell browser-sync which files have changed to inject them */
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('out/blog'));
+
+  /* split the stream into two paths */
+  var indexFile = posts
+  /* get infos from post json */
+    .pipe(through.obj(function (obj, _, callback) {
+      var json = JSON.parse(obj.contents);
+      json.url = 'blog/' + path.basename(obj.path, path.extname(obj.path)) + '.html';
+      this.push(json);
+      callback();
+    }))
+    /* buffer the stream of objects into a single array */
+    .pipe(gutil.buffer())
+    /* stringify that array */
+    .pipe(through.obj(function (arr, _, callback) {
+      // we could sort the array here...
+      this.push(JSON.stringify({posts : arr}));
+      callback();
+    }))
+    /* make a vinyl file out of the stringified array */
+    .pipe(source('blog.json'))
+    /* push the infos as json into an index template */
+    .pipe(wrap({src : 'src/blog/index-template.html'}))
+    /* use an html extension again */
+    .pipe(rename({extname : '.html'}))
+    /* write the file when done */
+    .pipe(gulp.dest('out/'));
+
+  /* merge the splitted streams again */
+  return merge(processedPosts, indexFile)
+  /* tell browser-sync which files have changed to inject them */
+    .pipe(browserSync.stream())
 }
 
 function buildSassTask() {
